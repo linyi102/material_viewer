8fd5c7a3ad2283a0087635dff354a6fe788e03e1
linyi517 <1158029271@qq.com>
2024-10-04 16:39:03 +0800

    feat: Toast 使用悬浮效果，宽屏时增加外边距

--- lib/utils/toast_util.dart
+++ lib/utils/toast_util.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class ToastUtil {
   static Future<void> showMessage(BuildContext context, String? message) async {
     if (!context.mounted || message == null || message.isEmpty) return;
 
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+    final device = getDeviceType(MediaQuery.of(context).size);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        margin: device == DeviceScreenType.mobile
+            ? null
+            : const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+        content: Text(message),
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
   }
 }
